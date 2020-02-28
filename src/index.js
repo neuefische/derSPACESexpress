31ff4cbfac0a3710d1d90a4962181f7b45ce27f3
Lydia <60884137+Lenkly@users.noreply.github.com>
2020-02-28 11:23:19 +0100

    Fixed performance troubles in index.hmtl (#19)
    * Fixed performance troubles in index.hmtl
    
    * Add build script in package.json

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 // Create data for import
-
-const teasers = [
-  {
-    title: "Wie Frankreich seine Bettwanzen-Plage bekämpft",
-    subtitle: "Panorama",
-    imglink:
-      "https://cdn.prod.www.spiegel.de/images/f07fe829-de28-40ab-8db1-91f2e3373ebf_w948_r1.77_fpx28.63_fpy44.98.jpg",
-    teasertext:
-      "Sie beißen Bürger und Touristen, lösen Juckreiz und großes Unbehagen aus: Frankreich hat ein Problem mit Bettwanzen.Im Kampf gegen das Ungeziefer schaltet sich nun der Staat ein.",
-    teasersource: "dpa",
-    teaserlink: "article.html"
-  },
-  {
-    title: "Erst der Mensch, dann der Sport, dann das Drumherum",
-    subtitle: "Malaika Mihambo",
-    imglink: "https://www.sueddeutsche.de/image/sz.1.4814389/600x337",
-    teasertext:
-      "Weitsprung-Weltmeisterin Malaika Mihambo scheint zu Beginn der neuen Leichtathletik- Saison fast alles zu gelingen.Auch, weil sie klare Prioritäten setzt.",
-    teasersource: "Von Johannes Knuth, Leipzig",
-    teaserlink: "article-2.html"
-  }
-];
+import { teasers } from "./data/teaser.js";
 
 function createTeaser(teasers) {
   /* define object from DOM where we want to append teasers */
@@ -35,7 +14,6 @@ function createTeaser(teasers) {
     let teaserSubtitle = document.createElement("div");
     let teaserTitle = document.createElement("h2");
     let teaserText = document.createElement("p");
-    let teaserSource = document.createElement("p");
     let teaserLink = document.createElement("a");
 
     /* insert content */
@@ -45,30 +23,26 @@ function createTeaser(teasers) {
     let titleContent = document.createTextNode(teaser.title);
     teaserTitle.appendChild(titleContent);
 
-    let teaserTextContent = document.createTextNode(teaser.teasertext);
+    let teaserTextContent = document.createTextNode(teaser.teaserText);
     teaserText.appendChild(teaserTextContent);
-
-    let teaserSourceContent = document.createTextNode(teaser.teasersource);
-    teaserSource.appendChild(teaserSourceContent);
 
     let teaserLinkContent = document.createTextNode("Mehr lesen...");
     teaserLink.appendChild(teaserLinkContent);
 
-    teaserLink.href = teaser.teaserlink;
+    teaserLink.href = teaser.teaserLink;
 
     /* build article structure */
     teaserArticle.appendChild(teaserImgWrapper);
     teaserArticle.appendChild(teaserSubtitle);
     teaserArticle.appendChild(teaserTitle);
     teaserArticle.appendChild(teaserText);
-    teaserArticle.appendChild(teaserSource);
     teaserArticle.appendChild(teaserLink);
 
     /* insert image into image wrapper */
     teaserImgWrapper.appendChild(teaserImg);
 
     /* insert image link into image */
-    teaserImg.src = teaser.imglink;
+    teaserImg.src = teaser.imgLink;
 
     /* append article to main element */
     main.appendChild(teaserArticle);
