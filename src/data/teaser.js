7b2d66586491899c81a993e83bd6f84091c81748
lenkly <lenk.lydia@gmail.com>
2020-02-28 11:05:43 +0100

    Fixed performance troubles in index.hmtl

--- src/data/teaser.js
+++ src/data/teaser.js
@@ -6,7 +6,7 @@ export const teasers = [
       "https://cdn.prod.www.spiegel.de/images/f07fe829-de28-40ab-8db1-91f2e3373ebf_w948_r1.77_fpx28.63_fpy44.98.jpg",
     teaserText:
       "Sie beißen Bürger und Touristen, lösen Juckreiz und großes Unbehagen aus: Frankreich hat ein Problem mit Bettwanzen.Im Kampf gegen das Ungeziefer schaltet sich nun der Staat ein.",
-    teaserlink: "article.html"
+    teaserLink: "article.html"
   },
   {
     title: "Erst der Mensch, dann der Sport, dann das Drumherum",
@@ -15,6 +15,6 @@ export const teasers = [
       "https://cdn.prod.www.spiegel.de/images/f07fe829-de28-40ab-8db1-91f2e3373ebf_w948_r1.77_fpx28.63_fpy44.98.jpg",
     teaserText:
       "Weitsprung-Weltmeisterin Malaika Mihambo scheint zu Beginn der neuen Leichtathletik- Saison fast alles zu gelingen.Auch, weil sie klare Prioritäten setzt.",
-    teaserlink: "article-2.html"
+    teaserLink: "article-2.html"
   }
 ];
