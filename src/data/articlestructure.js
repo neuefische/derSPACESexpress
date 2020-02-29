f0e9ac7390e143149388c2ea444b587e09e8d821
hauptdigital <marc@haupt.digital>
2020-02-29 16:33:05 +0100

    Add article structure for dynamic create elements

---
+++ src/data/articlestructure.js
@@ -0,0 +1,59 @@
+// Define html structure (elements)
+export const structure = {
+  teaser: {
+    article: {
+      element: "article",
+      classList: "card"
+    },
+    imgWrapper: {
+      element: "div",
+      classList: "card__img"
+    },
+    img: {
+      element: "img",
+      classList: "img-fluid"
+    },
+    subTitle: {
+      element: "div",
+      classList: "card__subtitle"
+    },
+    title: {
+      element: "h2",
+      classList: "card__title"
+    },
+    teaserText: {
+      element: "p",
+      classList: ""
+    },
+    source: {
+      element: "p",
+      classList: "card__source"
+    },
+    link: {
+      element: "a",
+      classList: "card__link"
+    }
+  },
+  bestofprint: {
+    article: {
+      element: "article",
+      classList: "best-of-print__content"
+    },
+    imgWrapper: {
+      element: "div",
+      classList: "best-of-print__img"
+    },
+    img: {
+      element: "img",
+      classList: "img-fluid"
+    },
+    title: {
+      element: "h2",
+      classList: "best-of-print__title"
+    },
+    teaserText: {
+      element: "p",
+      classList: "best-of-print__teaser"
+    }
+  }
+};
